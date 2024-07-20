d09b718c9861229ff61a97be1eeac5fde01d97fa
DineshPawar07 <dineshpawarr07@gmail.com>
2024-07-20 10:19:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const contactForm = document.getElementById('contact-form');
+
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(contactForm);
+        const name = formData.get('name');
+        const email = formData.get('email');
+        const message = formData.get('message');
+
+        const mailtoLink = `mailto:dineshpawarr07@gmail.com?subject=Contact from ${name}&body=Name: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
+        window.location.href = mailtoLink;
+    });
+});
